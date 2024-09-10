7aec64e389b6e0b9d1bcf9ddc0c058c577e4a1d2
Mariano Ravinale <mravinale@gmail.com>
2024-09-10 18:39:29 -0300

    github token

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require('dotenv').config();  // Cargar las variables del archivo .env
 
 // Configuración del repositorio
 const GITHUB_TOKEN = process.env.CI_TOKEN; // Reemplaza con tu token de acceso personal
-const REPO_URL = `https://mravinale:${GITHUB_TOKEN}@github.com/mravinale/dbmigrations.git`; // Reemplaza con tu repo y usuario
+console.log('Token:', GITHUB_TOKEN);
+const REPO_URL = `https://x-access-token:${GITHUB_TOKEN}@github.com/mravinale/dbmigrations.git`; // Reemplaza con tu repo y usuario
 const REPO_DIR = './backup';  // Carpeta donde clonarás o ya tienes el repo
 
 // Función para ejecutar el comando pg_dump y generar el archivo dump.sql
